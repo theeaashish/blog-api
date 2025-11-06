f7b20a1abf5c6bad4101d8ef01c80ad16ca8ec96
Aashish Joshi <theeaashish@gmail.com>
2025-11-06 06:08:08 +0000

    feat: add upload to cloudinary

--- src/middlewares/uploadBlogBanner.ts
+++ src/middlewares/uploadBlogBanner.ts
@@ -1,6 +1,8 @@
+import uploadToCloudinary from '../lib/cloudinary';
 import { logger } from '../lib/winston';
 import Blog from '../models/blog';
 import type { Request, Response, NextFunction } from 'express';
+import type { UploadApiErrorResponse } from 'cloudinary';
 
 const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2 MB
 
@@ -34,13 +36,46 @@ const uploadBlogBanner = (method: 'post' | 'put') => {
     try {
       const { blogId } = req.params;
       const blog = await Blog.findById(blogId).select('banner.publicId').exec();
-    } catch (error) {
-      res.status(500).json({
-        code: 'ServerError',
-        message: 'Internal Server Error',
-        error: error,
+
+      const data = await uploadToCloudinary(
+        req.file.buffer,
+        // blog?.banner.publicId.replace('blog-api/', ''),
+      );
+
+      if (!data) {
+        res.status(500).json({
+          code: 'ServerError',
+          message: 'Internal Server Error',
+        });
+        logger.error('Failed to upload Blog banner to Cloudinary', {
+          blogId,
+          publicId: blog?.banner.publicId,
+        });
+        return;
+      }
+
+      const newBanner = {
+        publicId: data.public_id,
+        url: data.secure_url,
+        width: data.width,
+        height: data.height,
+      };
+
+      logger.info('Blog banner uploaded successfully', {
+        blogId,
+        banner: newBanner,
       });
-      logger.error('Error while uploading Blog banner', error);
+
+      req.body.banner = newBanner;
+
+      next();
+    } catch (error: UploadApiErrorResponse | any) {
+      res.status(error.http_code).json({
+        code: error.http_code < 500 ? 'ValidationError' : 'ServerError',
+        message: error.message,
+      });
+
+      logger.error('Error while uploading Blog banner to cloudinary', error);
     }
   };
 };
